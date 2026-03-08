5058dfbad25f29e49f0e6d2fcc52a5fdd36fb05a
Silvio <silviovasconcelos91@gmail.com>
2026-03-08 11:56:41 +0100

    Project init

---
+++ src/main/kotlin/vasconcelos/silvio/volley/VolleyApplication.kt
@@ -0,0 +1,11 @@
+package vasconcelos.silvio.volley
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class VolleyApplication
+
+fun main(args: Array<String>) {
+    runApplication<VolleyApplication>(*args)
+}
---
+++ src/test/kotlin/vasconcelos/silvio/volley/VolleyApplicationTests.kt
@@ -0,0 +1,13 @@
+package vasconcelos.silvio.volley
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class VolleyApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
